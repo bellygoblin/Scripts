3de1d760f5c6d82fb56adc17dddd58c5c0cd7e8f
Bellygoblin <brian@mitchellfamily.me>
2024-02-22 17:34:14 -0500

    Many script updates and file cleanup

---
+++ FCA/EnableOutlookMobile.ps1
@@ -0,0 +1,6 @@
+﻿$User = Read-Host -Prompt 'Input the email you wish to disable mobile access for'
+
+Set-CASMailbox "$User" -EwsAllowList @{Add="Outlook-iOS/*","Outlook-Android/*"}
+Set-CASMailbox "$User" -OutlookMobileEnabled $true
+
+Write-Output "Mobile email access for $User has been enabled.`n"
